052c4c3955c16e2b8e6eef6550fd67aadde38564
texpage1 <72826162+texpage1@users.noreply.github.com>
2026-07-28 17:00:47 -0500

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -1,6 +1,6 @@
 // Filled during deployment. Keep enabled false until a private Supabase project is connected.
 window.LEGACY_IN_STONE_CLOUD = {
-  enabled: false,
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  enabled: true,
+  supabaseUrl: 'https://jvialuefznurzyxegbyi.supabase.co',
+  supabaseAnonKey: 'sb_publishable_hbDWtS8EMQIzP_d4D5beOQ_UwmpgIZT'
 };
